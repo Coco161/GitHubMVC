8415b264252c7ea1330d232f2fe0251d9dc5c5b8
Konstantinos Moustakas <moustakaskonstantinos@gmail.com>
2022-06-14 19:32:01 +0300

    Added Gig Modl

---
+++ GitHubMVC/Models/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GitHubMVC.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+
+        [Required] // Not Nullable
+        [StringLength(255)] // nvarchar(255)
+        public string Name { get; set; }
+
+    }
+}
